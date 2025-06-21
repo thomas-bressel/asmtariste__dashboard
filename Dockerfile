e7abc6cd179e8b543a3611af56639cef6b150291
thomas-bressel <tbressel@outlook.fr>
2025-06-21 15:22:29 +0200

    feat: Burger button change into  a cross button when click on it

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:22-alpine
+
+WORKDIR /app
+RUN npm install -g @angular/cli
+COPY package*.json ./
+RUN npm install
+COPY . .
+EXPOSE 4200
+CMD ["ng", "serve", "--host", "0.0.0.0"]
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Button } from '../ui/button/button';
 
 @Component({
   selector: 'header[app-header]',
   imports: [Button],
+  host: {
+    'class': 'header'
+  },
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+// async data attributes
+isNavigationOverlay = signal(false);
 
+
+
+  handleNavigationOverlay() {
+    this.isNavigationOverlay.set(!this.isNavigationOverlay());
+  }
 }
