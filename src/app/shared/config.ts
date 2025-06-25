5084e83abcf1f233d6d60dc7cb932ed37d3faf12
thomas-bressel <tbressel@outlook.fr>
2025-06-25 22:34:13 +0200

    cicd: update staging yamel cicd file

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,10 +1,10 @@
 const userApiEnv = {
     local: 'http://localhost:5002',
-    production: 'http://85.31.238.192:5002',
+    production: 'https://85.31.238.192:5002',
 }
 const contentApiEnv = {
     local: 'http://localhost:5001',
-    production: 'http://85.31.238.192:5001'
+    production: 'https://85.31.238.192:5001'
 }
 
 export const USER_API_URI: string = userApiEnv.production;
